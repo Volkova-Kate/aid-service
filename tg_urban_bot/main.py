d4415f608507071ca0c2ae2a9ada055c12ca6699
Katerina Volkova <e.volkova240401@gmail.com>
2025-01-30 01:13:09 +0300

    Added English to the interface-2

--- tg_urban_bot/main.py
+++ tg_urban_bot/main.py
@@ -19,30 +19,33 @@ async def cmd_start(message: Message):
         await registration(user_id)
         await free_bonus(user_id)
         await message.answer(
-            """Поздравляю, вы успешно прошли регистрацию! Теперь вам доступно 10 тестовых запросов, расширить их число вы сможете, оформив подписку."""
+            """Поздравляю, вы успешно прошли регистрацию! Теперь вам доступно 10 тестовых запросов, увеличить их число вы сможете, оформив подписку.\nCongratulations, you have successfully registered! There are now 10 test queries available to you, and you can increase their number by subscribing."""
         )
     await message.answer(
         """Укажите в одном сообщении тип работ, назначение и площадь объекта. Для внесения дополнительной информации отправьте дополненный запрос заново. 
-Вот пример запроса: Нужен проект интерьера лобби для офисного здания площадью 170 кв.м."""
+Вот пример запроса: Нужен проект интерьера лобби для офисного здания площадью 170 кв.м.
+
+Write in one message the type of work, the purpose and the area of the object. To add additional information, send the updated request again.
+Here is an example request: We need a lobby interior design for an office building with an area of 170sq.m."""
     )
 
 
 @dp.message(Command("bonus"))
-@description_meta("bonus", "Укажите пароль для получения бесплатных запросов")
+@description_meta("bonus", "Укажите пароль для получения бесплатных запросов.\nEnter the password for free requests.")
 async def bonus_command(message: Message):
     if len(inp := message.text.split(" ")) > 1:
         password = inp[1]
         await message.answer(await free_bonus(message.from_user.id, password))
     else:
-        await message.answer("Ошибка!\nВведите пароль в сообщении с командой!")
+        await message.answer("Ошибка!\nВведите пароль в сообщении с командой!\nWrong! Enter the password according to the command! ")
 
 
 @dp.message(Command("balance"))
-@description_meta("balance", "Ваш баланс и тип подписки")
+@description_meta("balance", "Ваш баланс и тип подписки\nYour balance and subscription type.")
 async def balance_command(message: Message):
     result = await check_user(message.from_user.id)
     await message.answer(
-        f"Подписка: {result['subscription']}\nКол-во запросов: {result['available_requests']}"
+        f"Подписка/subscription: {result['subscription']}\nКоличество запросов/number of requests: {result['available_requests']}"
     )
 
 
@@ -50,7 +53,13 @@ async def set_command_desc(bot: Bot):
     commands = [BotCommand(command=k, description=v) for k, v in COMMAND_LIST.items()]
     await bot.set_my_commands(commands)
     await bot.set_my_description(
-        """Привет, я ассистент aid! Я помогаю искать талантливых специалистов из мира дизайна и архитектуры. Просто напишите свой запрос, и я предложу вам подходящих специалистов."""
+        """Привет, я ассистент AID!
+Я помогаю искать талантливых специалистов из мира дизайна и архитектуры.
+Просто напишите свой запрос, и я предложу вам подходящих специалистов.
+
+Hi, I'm assistant AID!
+I help you find talented specialists from the world of design and architecture.
+Just write your request and I will suggest the right specialists for you."""
     )
 
 
